12b8e2aec8fe332e5212169452f91d1f77873596
cristianocccufcg <cristiano.silva@ccc.ufcg.edu.br>
2026-01-02 15:40:53 -0300

    Adicionado || caso o .env esteja ausente.

--- lab_init/src/config/database.ts
+++ lab_init/src/config/database.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT as "postgres" | "mysql",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  dialect: (process.env.DB_DIALECT as "postgres" | "mysql") || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || "lab-init",
+  password: process.env.DB_PASS || "web1232",
+  database: process.env.DB_NAME || "lab_init_web_II",
   logging: false
 });
 
