f6b984ed86317fcdadc6385bddb3226fa49437f8
cristianocccufcg <cristiano.silva@ccc.ufcg.edu.br>
2026-01-02 15:55:15 -0300

    Retirado dados sensíveis como senhas do código

--- lab_init/src/config/database.ts
+++ lab_init/src/config/database.ts
@@ -7,9 +7,9 @@ const sequelize = new Sequelize({
   dialect: (process.env.DB_DIALECT as "postgres" | "mysql") || "postgres",
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || "lab-init",
-  password: process.env.DB_PASS || "web1232",
-  database: process.env.DB_NAME || "lab_init_web_II",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   logging: false
 });
 
