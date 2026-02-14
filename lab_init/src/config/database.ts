aff5dacfa318e98685c5c93039f88801ae76d559
cristianocccufcg <cristiano.silva@ccc.ufcg.edu.br>
2026-02-14 17:50:19 -0300

    "Ajuste"

--- lab_init/src/config/database.ts
+++ lab_init/src/config/database.ts
@@ -7,8 +7,11 @@ dotenv.config();
  * Instância do Sequelize configurada para conexão com o banco de dados.
  * Utiliza variáveis de ambiente para definir as credenciais e parâmetros de conexão.
  */
+const isTest = process.env.NODE_ENV === 'test';
+
 const sequelize = new Sequelize({
-  dialect: (process.env.DB_DIALECT as "postgres" | "mysql") || "postgres",
+  dialect: isTest ? 'sqlite' : ((process.env.DB_DIALECT as "postgres" | "mysql") || "postgres"),
+  storage: isTest ? ':memory:' : undefined,
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER,
